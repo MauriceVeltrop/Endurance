84dcb40509de131e457cfbea9c70bef7e8d80c6e
MauriceVeltrop <m.veltrop@gmail.com>
2026-05-12 11:21:55 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
