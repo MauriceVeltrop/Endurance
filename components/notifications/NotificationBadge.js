fc0969f62c5c01d40100f0841874f6ab29e73e82
MauriceVeltrop <m.veltrop@gmail.com>
2026-05-18 12:17:44 +0200

    Add files via upload

--- components/notifications/NotificationBadge.js
+++ components/notifications/NotificationBadge.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export default function NotificationBadge({ className = "" }) {
   const [count, setCount] = useState(0);
